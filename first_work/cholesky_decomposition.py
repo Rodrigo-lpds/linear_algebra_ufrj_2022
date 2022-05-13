293128bb6516e7b05bbf0d24d6ca8cfe7e7c07d6
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-05-13 20:05:58 -0300

    add cholesky algorithm

--- first_work/cholesky_decomposition.py
+++ first_work/cholesky_decomposition.py
@@ -1,4 +1,17 @@
-def decompose(A):
-  '''
-    
-  '''
+import math
+
+A = [[1,0.2,0.4],[0.2,1,0.5],[0.4,0.5,1]]
+
+for i in range(len(A)):
+  generic_sum = 0 
+  for k in range(0,i):
+    generic_sum += (A[i][k])**2
+  
+  A[i][i] = math.sqrt(A[i][i] - generic_sum)
+  for j in range(i+1,len(A)):
+    generic_sum = 0
+    for k in range(0,i):
+      generic_sum+= (A[i][k] * A[j][k])
+    A[j][i] = (1/A[i][i]) * (A[i][j] - generic_sum)
+
+print(A)
