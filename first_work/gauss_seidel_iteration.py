7477bc65f65462a58a90f850aeff1d5600caf643
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-05-15 17:06:49 -0300

    add basic structure of gauss seidel iteration

--- first_work/gauss_seidel_iteration.py
+++ first_work/gauss_seidel_iteration.py
@@ -4,23 +4,27 @@
 # calculate normal 
 # split sum loops into separate functions
 # check if iteration can be done
-
-
-import math 
+import math
 
 A = [[3,-1,-1],[-1,3,-1],[-1,-1, 3]]
 B = [1,2,1]
 X_0 = [1,1,1]
 X_1 = [0,0,0]
 R = 0
-for solution in range((1)):
-  for i in range(len(X_0)):
-    generic_sum = 0
-    for j in range(0,len(X_0)):
-        if j != i:
-          generic_sum += A[i][j] * X_0[j]
-    X_1[i] = (B[i] - generic_sum) / A[i][i]
 
+for solutions in range(1):
+  for i in range(len(X_0)):
+    sum_with_new_X = 0
+    sum_with_old_X = 0
+    for j in range(i):
+      sum_with_new_X += A[i][j] * X_1[j]
+    for j in range(i+1,len(A)):
+      sum_with_old_X += A[i][j] * X_0[j]
+
+    X_1[i] = (B[i]-sum_with_new_X - sum_with_old_X) / A[i][i]
+  #print(solutions)
+  #if solutions+1 != 2:
+  #  X_0 = X_1
 
 def calculate_normal(X_0,X_1):
   sum_0 = 0
@@ -33,3 +37,4 @@ def calculate_normal(X_0,X_1):
 
 print(X_1)
 print(calculate_normal(X_0,X_1))
+
