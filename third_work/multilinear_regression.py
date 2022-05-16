e291b94fbcf16f6054d839a2c395a1c79cc9fa39
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-05-16 01:10:13 -0300

    add basic structure for multilinear regression

---
+++ third_work/multilinear_regression.py
@@ -0,0 +1,20 @@
+#PTPB=PTY
+import sys
+from unicodedata import decomposition
+sys.path.insert(0,"..")
+
+from first_work.lu_decomposition import LU
+#coordinates 
+X = [-2,0,1]
+Y = [-27,-1,0]
+n = 3
+P = [[0 for i in range(n)] for j in range(n)]
+P = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+
+for i in range(n):
+  for j in range(n):
+    P[i][j] = X[i]**j
+
+decomposition = LU(P, Y)
+B = decomposition.solution()
+print(B)
