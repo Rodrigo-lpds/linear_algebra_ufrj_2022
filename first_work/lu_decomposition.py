8002d71c9a1b1638954fe382c7717b41c3d6bc90
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-05-16 00:17:53 -0300

    add backward and forward substitution

--- first_work/lu_decomposition.py
+++ first_work/lu_decomposition.py
@@ -21,7 +21,7 @@ def decompose(A):
       for i in range(k+1,n):
         A[i][j] = A[i][j] - A[i][k]*A[k][j]
 
-  print(A)
+  #print(A)
   
   #compose L
   L = [[1,0,0],[0,1,0],[0,0,1]]
@@ -29,12 +29,45 @@ def decompose(A):
     for j in range(i):
       L[i][j] = A[i][j]
 
-  print(L)
+  #print(L)
   
   # compose U
   U = [A[0],[0,0,0],[0,0,0]]
   for i in range(1,n):
     for j in range(i,n):
       U[i][j] = A[i][j]
+  return L,U
 
-  print(U)
+def forward_substitution(L,B):
+  n = len(B)
+  Y = [0]*n
+  Y[0] = B[0]/L[0][0]
+  
+  for i in range(1,n):
+    sum = 0
+#    print(Y)
+    for j in range(i):
+      sum+= L[i][j] * Y[j]
+    Y[i] = (B[i]-sum)/L[i][i]
+  return Y
+
+def backward_substitution(Y,U):
+  n = len(Y)
+  X = [0] * n
+  X[n - 1] = Y[n - 1]/U[n - 1][n - 1]
+
+  for i in range(n-2,-1,-1):
+    sum = 0
+    for j in range(i+1,n):
+      sum+= U[i][j] * X[j]
+    X[i] = (Y[i] - sum)/U[i][i]
+  
+  return X
+
+A = [[1,2,2],[4,4,2],[4,6,4]]
+B = [3,6,10]
+L,U = decompose(A)
+Y = forward_substitution(L,B)
+
+X = backward_substitution(Y,U)
+print(X)
