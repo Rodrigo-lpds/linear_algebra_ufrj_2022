3eb75dbda3e34672b69a8d8709b3405cf73abaa8
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-05-16 21:53:36 -0300

    turn multilinear regression into a class

--- third_work/multilinear_regression.py
+++ third_work/multilinear_regression.py
@@ -4,22 +4,30 @@ from unicodedata import decomposition
 sys.path.insert(0,"..")
 
 from first_work.lu_decomposition import LU
-#coordinates 
-X = [1,2.5,4]
-Y = [2,3.5,8]
-n = 3
-P = [[0 for i in range(n)] for j in range(n)]
 
-for i in range(n):
-  for j in range(n):
-    P[i][j] = X[i]**j
+class MultilinearRegression:
+  def __init__(self,A,x):
+    self.A = A
+    self.x = x
+    #coordinates 
+    self.n = len(A[0])
+    self.X = self.A[0]
+    self.Y = self.A[1]
 
-decomposition = LU(P, Y)
-B = decomposition.solution()
-print(B)
+  def build_matrix(self):
+    P = [[0 for i in range(self.n)] for j in range(self.n)]
 
-x = 3.25
-estimate_value = B[0]
-for i in range(1,len(B)):
-  estimate_value += B[i]* x**i
-print(estimate_value)
+    for i in range(self.n):
+      for j in range(self.n):
+        P[i][j] = self.X[i]**j
+
+    decomposition = LU(P, self.Y)
+    return decomposition.solution()
+  
+  def estimate_value(self):
+    B = self.build_matrix()
+    estimate_value = B[0]
+    for i in range(1,len(B)):
+      estimate_value += B[i]* self.x**i
+    
+    return estimate_value
