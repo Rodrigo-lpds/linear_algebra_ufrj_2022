73142ad1d4fe353c65b858a7522e781f9642d504
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-05-16 21:53:22 -0300

    turn lagrange interpolation into a class

--- third_work/lagrange_interpolation.py
+++ third_work/lagrange_interpolation.py
@@ -1,35 +1,43 @@
-#coordinates 
-X = [1,2.5,4]
-Y = [2,3.5,8]
-n = 3
-P = [[0 for i in range(n)] for j in range(n)]
+class LagrangeInterpolation:
+  def __init__(self,A,x):
+    self.A = A
+    self.x = x
+    #coordinates 
+    self.n = len(A[0])
+    self.X = self.A[0]
+    self.Y = self.A[1]
 
-# (x-xi) * (x-xj) = (x**2 -x*xj - x*xi + xi*xj)
-# [xi*xj, -x*xj - x*xi, x**2]
-# [[0, -1, 1],[-2,1,1],[0,2,1]]
-#make the equation matrix
-for i in range(n):
-  P[i][0] = 1 # for the multiply below doesn't zeroed
-  divisor = 1 
-  for k in range(n): #Dividend Product 
-    if i != k:
-      P[i][0]  *= X[k] 
-      P[i][1]  += -(X[k])
-      P[i][2]  = 1
-      divisor  *= (X[i]- X[k]) 
-  for k in range(n): #Divisor Product
-    P[i][k]  = P[i][k]/divisor * Y[i]
+  def build_matrix(self):
+    P = [[0 for i in range(self.n)] for j in range(self.n)]
 
-#sum variables to get the polinomial equation
-B = [0 for i in range(n)]
-for i in range(n):
-  for j in range(n):
-    B[i] += P[j][i]
+    # (x-xi) * (x-xj) = (x**2 -x*xj - x*xi + xi*xj)
+    # [xi*xj, -x*xj - x*xi, x**2]
+    # [[0, -1, 1],[-2,1,1],[0,2,1]]
+    #make the equation matrix
+    for i in range(self.n):
+      P[i][0] = 1 # for the multiply below doesn't zeroed
+      divisor = 1 
+      for k in range(self.n): #Dividend Product 
+        if i != k:
+          P[i][0]  *= self.X[k] 
+          P[i][1]  += -(self.X[k])
+          P[i][2]  = 1
+          divisor  *= (self.X[i]- self.X[k]) 
+      for k in range(self.n): #Divisor Product
+        P[i][k]  = P[i][k]/divisor * self.Y[i]
 
-print (B)
-x = 3.25
-estimate_value = B[0]
-for i in range(1,len(B)):
-  estimate_value += B[i]* x**i
-#print(B[0] + B[1]*3.25 + B[2] * 3.25**2)
-print (estimate_value)
+    #sum variables to get the polinomial equation
+    B = [0 for i in range(self.n)]
+    for i in range(self.n):
+      for j in range(self.n):
+        B[i] += P[j][i]
+    return B
+
+
+  def estimate_value(self):
+      B = self.build_matrix()
+      estimate_value = B[0]
+      for i in range(1,len(B)):
+        estimate_value += B[i]* self.x**i
+      
+      return estimate_value
