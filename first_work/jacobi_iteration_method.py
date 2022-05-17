28edb80269966f159d41c714f7d4c181a7d13c03
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-05-16 22:24:30 -0300

    turn jacobi iteration into a class

--- first_work/jacobi_iteration_method.py
+++ first_work/jacobi_iteration_method.py
@@ -4,32 +4,34 @@
 # calculate normal 
 # split sum loops into separate functions
 # check if iteration can be done
-
-
 import math 
 
-A = [[3,-1,-1],[-1,3,-1],[-1,-1, 3]]
-B = [1,2,1]
-X_0 = [1,1,1]
-X_1 = [0,0,0]
-R = 0
-for solution in range((1)):
-  for i in range(len(X_0)):
-    generic_sum = 0
-    for j in range(0,len(X_0)):
-        if j != i:
-          generic_sum += A[i][j] * X_0[j]
-    X_1[i] = (B[i] - generic_sum) / A[i][i]
+class JacobiIterationMethod:
+  def __init__(self,n,A,B,TOLm):
+    self.n = n
+    self.A = A
+    self.B = B
+    self.TOLm = TOLm
+    self.X_0 = [1,1,1]
+    self.X_1 = [0,0,0]
+    self.R = 0
+
+  def calculate_solutions(self):
+    for i in range(len(self.X_0)):
+      generic_sum = 0
+      for j in range(0,len(self.X_0)):
+          if j != i:
+            generic_sum += self.A[i][j] * self.X_0[j]
+      self.X_1[i] = (self.B[i] - generic_sum) / self.A[i][i]
+
+    return self.X_1
 
 
-def calculate_normal(X_0,X_1):
-  sum_0 = 0
-  sum_1 = 0
-  for i in range(0,len(X_0)):
-    sum_0 += (X_1[i] - X_0[i])**2
-    sum_1 += X_1[i] ** 2
-  
-  return math.sqrt(sum_0)/math.sqrt(sum_1)
-
-print(X_1)
-print(calculate_normal(X_0,X_1))
+  def calculate_normal(X_0,X_1):
+    sum_0 = 0
+    sum_1 = 0
+    for i in range(0,len(X_0)):
+      sum_0 += (X_1[i] - X_0[i])**2
+      sum_1 += X_1[i] ** 2
+    
+    return math.sqrt(sum_0)/math.sqrt(sum_1)
