ae1fa067c1dc7e894075978b9ce1363ab6bb8613
Rodrigo Lucas <rodrigo.lpdsilva@gmail.com>
2022-05-16 22:24:17 -0300

    turn gauss seidel into a class

--- first_work/gauss_seidel_iteration.py
+++ first_work/gauss_seidel_iteration.py
@@ -6,35 +6,36 @@
 # check if iteration can be done
 import math
 
-A = [[3,-1,-1],[-1,3,-1],[-1,-1, 3]]
-B = [1,2,1]
-X_0 = [1,1,1]
-X_1 = [0,0,0]
-R = 0
+class GaussSeidelIteration:
+  def __init__(self,n,A,B,TOLm):
+    self.n = n
+    self.A = A
+    self.B = B
+    self.TOLm = TOLm
+    self.X_0 = [1,1,1]
+    self.X_1 = [0,0,0]
+    self.R = 0
 
-for solutions in range(1):
-  for i in range(len(X_0)):
-    sum_with_new_X = 0
-    sum_with_old_X = 0
-    for j in range(i):
-      sum_with_new_X += A[i][j] * X_1[j]
-    for j in range(i+1,len(A)):
-      sum_with_old_X += A[i][j] * X_0[j]
+  def calculate_solutions(self):
+    for i in range(len(self.X_0)):
+      sum_with_new_X = 0
+      sum_with_old_X = 0
+      for j in range(i):
+        sum_with_new_X += self.A[i][j] * self.X_1[j]
+      for j in range(i+1,len(self.A)):
+        sum_with_old_X += self.A[i][j] * self.X_0[j]
 
-    X_1[i] = (B[i]-sum_with_new_X - sum_with_old_X) / A[i][i]
+      self.X_1[i] = (self.B[i]-sum_with_new_X - sum_with_old_X) / self.A[i][i]
+    
+    return self.X_1
   #print(solutions)
   #if solutions+1 != 2:
   #  X_0 = X_1
-
-def calculate_normal(X_0,X_1):
-  sum_0 = 0
-  sum_1 = 0
-  for i in range(0,len(X_0)):
-    sum_0 += (X_1[i] - X_0[i])**2
-    sum_1 += X_1[i] ** 2
-  
-  return math.sqrt(sum_0)/math.sqrt(sum_1)
-
-print(X_1)
-print(calculate_normal(X_0,X_1))
-
+  def calculate_normal(self):
+    sum_0 = 0
+    sum_1 = 0
+    for i in range(0,len(self.X_0)):
+      sum_0 += (self.X_1[i] - self.X_0[i])**2
+      sum_1 += self.X_1[i] ** 2
+    
+    return math.sqrt(sum_0)/math.sqrt(sum_1)
